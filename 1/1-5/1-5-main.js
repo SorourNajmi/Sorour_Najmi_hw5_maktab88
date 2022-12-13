3e941353a3a5703154892066e20dbf52af639b8f
Sorour Najmi <sorour.najmi@gmail.com>
2022-12-13 19:29:38 +0330

    Changed and done

--- 1/1-5/1-5-main.js
+++ 1/1-5/1-5-main.js
@@ -1,16 +1,16 @@
-console.log(printPeerToPeer(["a-b-c-d", "a-b-f-g", "y-n-l-k", "m-o-p-j", "v-q-w-e", "x-z-p-j"]));
+console.log(printPeerToPeer(["a-b-c-d", "a-b-f-g-x", "y-n-l-k", "m-o-p-j-q", "v-q-w-e", "x-z-p-j"]));
 
 function printPeerToPeer(arr) {
     if (!Array.isArray(arr)) throw new Error("Not an array!");
     let result = [];
-    let len = (arr[0].split('-')).length;
+    // let len = (arr[0].split('-')).length;
     for (let item of arr) {
         if (typeof(item) !== 'string') throw new Error("Not a string!");
         const itemArr = item.split('-');
-        if (itemArr.length !== len) throw new Error("Invalid size!");
-        for (let i = 0; i < len; i++) {
-            if (result[i] === undefined) result.push([]);
-            if (!result[i].includes(itemArr[i])) result[i].push(itemArr[i]);
+        // if (itemArr.length !== len) throw new Error("Invalid size!");
+        for (let i = 0; i < itemArr.length; i++) {
+            if (result[i] === undefined)  result.push([]);
+            if (!result[i].includes(itemArr[i]))  result[i].push(itemArr[i]);
         }
     }
     return result;
